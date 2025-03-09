868400a03bab2859eda02d97e59b91276c99fe21
TG <ebiritg@gmail.com>
2025-03-09 23:43:39 +0100

    Handle step one

---
+++ ksh.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"os/exec"
+
+	//"log"
+	"strings"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Printf("ksh> ")
+		if !scanner.Scan() {
+			break
+		}
+
+		trimmed := strings.TrimSpace(scanner.Text())
+		if trimmed == "" {
+			continue
+		}
+
+		split := strings.Fields(trimmed)
+		if split[0] == "exit" {
+				return
+		}
+		cmd := exec.Command(split[0], split[1:]...)
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+
+		if err := cmd.Run(); err != nil {
+		fmt.Println(err.Error())
+		}
+	}
+}
